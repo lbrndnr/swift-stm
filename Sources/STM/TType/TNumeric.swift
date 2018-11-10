de1636eab9faea135ebd8fe80eec5db8918d9a6a
Laurin Brandner <mail@laurinbrandner.ch>
2018-11-10 10:38:33 +0100

    Add all numeric types

--- Sources/STM/TType/TNumeric.swift
+++ Sources/STM/TType/TNumeric.swift
@@ -9,7 +9,17 @@
 import Foundation
 
 public typealias TInt = Ref<Int>
+public typealias TInt8 = Ref<Int8>
+public typealias TInt16 = Ref<Int16>
+public typealias TInt32 = Ref<Int32>
+public typealias TInt64 = Ref<Int64>
+public typealias TUInt8 = Ref<UInt8>
+public typealias TUInt16 = Ref<UInt16>
+public typealias TUInt32 = Ref<UInt32>
+public typealias TUInt64 = Ref<UInt64>
+
 public typealias TFloat = Ref<Float>
+public typealias TDouble = Ref<Double>
 
 public func +<T: Numeric>(lhs: Ref<T>, rhs: Ref<T>) -> Ref<T> {
     return Ref(lhs.get() + rhs.get())
