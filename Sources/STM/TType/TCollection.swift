59a5d512623a5b207bb7cd88b6c5fcc38b4934b1
Laurin Brandner <mail@laurinbrandner.ch>
2019-01-02 11:54:34 +0100

    Improve TSequence

--- Sources/STM/TType/TCollection.swift
+++ Sources/STM/TType/TCollection.swift
@@ -8,12 +8,23 @@
 import Foundation
 
 //extension Reference: Collection where Value: Collection {
-
-//    public typealias Element = Value.Element
-//    public typealias Iterator = Value.Iterator
 //    
-//    public func makeIterator() -> Value.Iterator {
-//        return get().makeIterator()
+//    public typealias Index = Value.Index
+//    
+//    public var startIndex: Index {
+//        return get().startIndex
+//    }
+//    
+//    public var endIndex: Index {
+//        return get().endIndex
+//    }
+//    
+//    public func index(after i: Index) -> Index {
+//        return get().index(after: i)
+//    }
+//    
+//    public subscript(position: Index) -> Value.Element {
+//        return get()[position]
 //    }
 //    
 //}
--- Sources/STM/TType/TSequence.swift
+++ Sources/STM/TType/TSequence.swift
@@ -8,13 +8,15 @@
 import Foundation
 
 public typealias TArray<E> = Ref<Array<E>>
+public typealias TSet<E> = Ref<Set<E>> where E: Hashable
+public typealias TDictionary<K, E> = Ref<Dictionary<K, E>> where K: Hashable, E: Hashable
 
 extension Reference: Sequence where Value: Sequence {
     
     public typealias Element = Value.Element
     public typealias Iterator = Value.Iterator
     
-    public func makeIterator() -> Value.Iterator {
+    public func makeIterator() -> Iterator {
         return get().makeIterator()
     }
     
